98930ca0391bfa5c7867eb02caaf7c773819b9a6
Benjamín Vivanco Andaur <85044908+Bangui1@users.noreply.github.com>
2021-10-11 22:45:39 -0300

    Add files via upload

---
+++ home.py
@@ -0,0 +1,10 @@
+class Home:#clase con interfaz una vez iniciada sesion/quiza tengamos que hacer una para admin tambien
+    #para una vez que se hace login
+    #def contactoDeInteres():
+    #def reportEvent():
+    #si es admin tambien puede usar
+    #def blockUser():
+    #def unblockUser():
+    #def confirmEvent():
+    #posible agregar un addAdmin() y kickAdmin()
+    pass
---
+++ login.py
@@ -0,0 +1,7 @@
+class Login:
+    #def register(): #usar manejo de archivos para poder crear usuarios y escribirlos en un .txt o .csv(usando pandas)
+    #def login(): #leer y buscar en el archivo donde se guardan los usuarios
+    #def entrar_como_sensor(): #crear un acceso especial sin login para los sensores
+    pass
+
+
