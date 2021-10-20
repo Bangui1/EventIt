4b68eeb9d00aecf5c7928713f302c3115d48ed8d
ChinoCoder <91900687+ChinoCoder@users.noreply.github.com>
2021-10-20 20:49:45 -0300

    Create datasets.py

---
+++ Classes/datasets.py
@@ -0,0 +1,10 @@
+class datasetANSES:
+  ciudadanos = []
+  
+class datasetBLOQUEADOS:
+  ciudadanos_bloqueados = []
+  
+class datasetUSUARIOS:
+  administradores = []
+  ciudadanos_user = []
+  users_totales = administradores + ciudadanos_user
