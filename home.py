8b625103a4dbce3f9e220f5c92e2e2878ed5a137
AlanFierr0 <85326611+AlanFierr0@users.noreply.github.com>
2021-10-20 00:41:35 -0300

    Update home.py

--- home.py
+++ home.py
@@ -1,10 +1,12 @@
-class Home:#clase con interfaz una vez iniciada sesion/quiza tengamos que hacer una para admin tambien
-    #para una vez que se hace login
-    #def contactoDeInteres():
-    #def reportEvent():
+class Home:
+    #clase con interfaz una vez iniciada sesion/quiza tengamos que hacer una para admin tambien para una vez que se hace login
+    #def contactoDeInteres():  def accept_request(usuario): teoricamente son lo mismo, agregar al contacto a la lista de contactos de interes
+    #def reportEvent(): solicitud para agregar evento a la listaEventos
     #si es admin tambien puede usar
-    #def blockUser():
-    #def unblockUser():
-    #def confirmEvent():
+    #def request(usuario): solicitud para agregar a la lista de contactos de interes
+    #def refuse_request(usario): cada vez que se rechaza un user sumar +1  
+    #def blockUser(user):   chequear con la funcion refuse_request() si la cantidad es >= 5, si ocurre esa condicion bloquear
+    #def unblockUser(usuario): chequear si el usuario esta bloqueado con usuario.isBlocked() , si ocurre esa condicionluego desbloquearlo
+    #def confirmEvent(): agregar un valor al evento donde confirma para agrandar la cantidad de participanted de dicho evento y hacerlo "mas rojo"
     #posible agregar un addAdmin() y kickAdmin()
     pass
