d4137bb5f66e265e1d5061b4f6bf446aeb5ab755
AlanFierr0 <85326611+AlanFierr0@users.noreply.github.com>
2021-10-20 00:39:50 -0300

    Update event.py

--- Classes/event.py
+++ Classes/event.py
@@ -2,7 +2,15 @@ class event:
     def __init__(self, tipo, coords):
         self.tipo = tipo
         self.coords = coords #estilo (a,b) o como se maneje el mapa
+    
+    def getType(self):
+        return self.type
 
 class sensor:
-    #def getInfo(event):
-    pass
+    # def __init__(self):
+    #     self.listaSensores = [] #esto podria ir en el login 
+   
+    def getInfo(self,event): 
+        for i in self.listaSensores:
+            if i == event:
+                return event.getType()
