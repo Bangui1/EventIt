a687fed420ec151e0cf78c7903ca78dd9e74f0b2
AlanFierr0 <85326611+AlanFierr0@users.noreply.github.com>
2021-10-20 00:41:13 -0300

    Update login.py

--- login.py
+++ login.py
@@ -1,7 +1,10 @@
 class Login:
     #def register(): #usar manejo de archivos para poder crear usuarios y escribirlos en un .txt o .csv(usando pandas)
     #def login(): #leer y buscar en el archivo donde se guardan los usuarios
+    #  for i in lstUsers
+    #       if usuario and contraseña == i
+    #           #entra al programa
     #def entrar_como_sensor(): #crear un acceso especial sin login para los sensores
+    #  entra al programa pero solo tiene habilitado la funcion de getInfo()
     pass
-
 
