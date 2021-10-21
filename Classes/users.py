25db3b6abb62d59d8b340896ba442386bc0ece67
ChinoCoder <91900687+ChinoCoder@users.noreply.github.com>
2021-10-20 21:51:55 -0300

    Update users.py

--- Classes/users.py
+++ Classes/users.py
@@ -1,20 +1,53 @@
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, classmethod
 class Usuario(ABC):
-    def __init__(self, CUIL, password, telefono,blocked,friends):
-        self.cuil = CUIL
+    def __init__(self, username, password):
         self.password = password
-        self.telefono = telefono
-        self.blocked = []
-        self.friends = []
+        self.username = username
+        self.notificaciones = []
+        
 
 class Admin(Usuario): 
     #no se registra en el sistema, se crea con login y contraseña predeterminada
-    #metodo para habilitar eventos
-    #bloquear y desbloquear ciudadanos
-    pass
+    notifsEventos = []
+    
+    def banUser(self, usuario):
+        #buscar en ambos datasets de usuarios y bloqueados - checkear BLockedState - de ser posible cambiarlo a True y agregar a dataset de bloqueados
+        pass
+    
+    def unbanUser(self, usuario):
+        pass
+    
+    @classmethod
+    def confirmEvent(self, event):
+        #chequear en lista de notifs x los eventos y aceptar para que aparezca para los sensores - quizas se puede armar la lista en orden de mas solicitudes
+        pass
+    
+    @classmethod
+    def denyEvent(self, event):
+        pass
 
 class Ciudadano(Usuario):
-    #se registra en el sistema
-    # o se logea en el sistema
-    pass
-  
+    #se registra en el sistema mediante Register() en Home
+    def __init__(self, username, password, CUIL, telefono):
+        Usuario.__init__(username, password)
+        self.cuil = CUIL
+        self.telefono = telefono
+        self.friends = []
+        self.NoOfTimesRejected = 0
+        self.pendingRequests = []
+        self.BlockedState = False
+        
+    def requestFriend(self, CUIL):
+        #enviar notificacion y agregar a pendingRequests del otro usuario con opcion de aceptar o denegar
+        pass
+    
+    def addFriend(self, username):
+        #se elimina la notif y se agrega el usuario a lista friends
+        pass
+    
+    def refuseFriend(self, username):
+        # +1 a NoOfTimesRejected del usuario -> un check si es >5 - si es cambiar BlockedState a True y agregar a dataset de bloqueados
+        pass
+    
+    def report_event(self): #completar los parentesis con tipo de evento para crearlo - despues enviar notificacion a administradores para aceptar/denegar
+        pass  
