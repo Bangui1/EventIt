6d4a3786b052197e370181749c3b978ea467ee32
ChinoCoder <91900687+ChinoCoder@users.noreply.github.com>
2021-10-25 20:51:36 -0300

    Update login.py

--- login.py
+++ login.py
@@ -1,11 +1,28 @@
 import pandas
+from Classes.datasets import datasetANSES, datasetUSUARIOS, datasetBLOQUEADOS
+from Classes.users import Usuario, Admin, Ciudadano, Sensor
 class Login:
-    #def register(): #usar manejo de archivos para poder crear usuarios y escribirlos en un .txt o .csv(usando pandas)
-    #def login(): #leer y buscar en el archivo donde se guardan los usuarios
-    #for i in lstUsers #lstUsers va a ser un csv que tenga en cada valor un usuario y su contraseña
-    #       if usuario and contraseña == i
-    #           #entra al programa
-    #def entrar_como_sensor(): #crear un acceso especial sin login para los sensores
-    #  entra al programa pero solo tiene habilitado la funcion de getInfo()
-    pass
+    def register(celular, CUIL, username, password): #usar manejo de archivos para poder crear usuarios y escribirlos en un .txt o .csv(usando pandas)
+        for n in datasetANSES.ciudadanos:
+            if n.cuil == CUIL:
+                user = Ciudadano(username, password, CUIL, celular)
+                datasetUSUARIOS.ciudadanos_user.append(user)
+            else:
+                return "No se encontró el cuil en la base de datos"
+    def login(username, password): #leer y buscar en el archivo donde se guardan los usuarios
+    for i in datasetUSUARIOS.ciudadanos_user: #lstUsers va a ser un csv que tenga en cada valor un usuario y su contraseña
+        if username == i.username and password == i.password
+            #login como usuario comun
+        else:
+            for i in datasetUSUARIOS.administradores:
+                if username == i.username and password == i.password
+                    #login como admin
+            else:
+                return "Usuario o contraseña incorrectos. Reescribir y reintentar"
+    def entrar_como_sensor(numero): #crear un acceso especial sin login para los sensores
+        for s in datasetUSUARIOS.sensores:
+            if s.num == numero:
+                #login como sensor - otra interfaz que no es como la de usuario - solo tiene habilitado la funcion de getInfo()
+            else:
+                return "ese sensor no existe"
 
