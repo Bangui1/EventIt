8995cf5b57c678d3b8dd90923f5fd9701af1dd39
Benjamín Vivanco Andaur <85044908+Bangui1@users.noreply.github.com>
2021-11-04 20:02:46 -0300

    register, login, admin agregado

--- login.py
+++ login.py
@@ -27,6 +27,7 @@
 #                 return "ese sensor no existe"
 
 #login usando archivos .csv 
+from csv import writer
 
 class Start:
     def Login(self):
@@ -34,11 +35,11 @@ class Start:
         while not log:
             username = input('Enter Username: ').rstrip()
             password = input('Enter Password: ')
-            with open('Usuarios_registrados.csv', 'r') as database:
+            with open('User_database.csv', 'r') as database:
                 for line in database:
                     row = line.strip().split(',')
-                    if username == row[0].strip():
-                        if password == row[1].strip():
+                    if username == row[2].strip():
+                        if password == row[3].strip():
                             log = True
                         else:
                             print('wrong password, please try again.')
@@ -50,8 +51,60 @@ class Start:
         register = False
         while not register:
             cuil = input('Please enter your CUIL: ')
-            with open('dataset_Anses.csv', 'r') as database:
+            phone_number = input('Please enter your phone number: ')
+            username = input('Enter your username: ')
+            
+            check_password = False
+            while not check_password:
+                password = input('Enter your password: ')
+                confirm_password = input('Please re-enter your password:')
+                if password == confirm_password:
+                    check_password = True
+                else:
+                    print('Passwords do not match, please try again.')
+
+
+            with open('dataset_Anses.csv', 'r', newline = '') as database:
                 for line in database:
                     row = line.strip().split(',')
                     if cuil == row[0]:
-                        register = True
+                        with open('User_database.csv', 'a') as user_database:
+                            user_data = [cuil, phone_number, username, password]
+                            data_writer = writer(user_database, lineterminator='\r')
+                            data_writer.writerow(user_data)
+                            register = True
+                            break
+            if register:
+                print('successful registration')
+            else:
+                print('CUIL not found in dataset, please try again')
+
+
+    def LoginAdmin(self):
+        log = False
+        while not log:
+            username = input('Enter Username: ').rstrip()
+            password = input('Enter Password: ')
+            with open('Admin_dataset.csv', 'r') as database:
+                for line in database:
+                    row = line.strip().split(',')
+                    if username == row[0].strip():
+                        if password == row[1].strip():
+                            log = True
+                        else:
+                            print('wrong password, please try again.')
+                    else:
+                        print('Username Not found')
+
+
+    def LoginSensor(self):
+        log = False
+        while not log:
+            username = input('Enter Area Code: ').rstrip()
+            with open('Sensor_dataset.csv', 'r') as database:
+                for line in database:
+                    row = line.strip().split(',')
+                    if username == row[0].strip():
+                        log = True
+                    else:
+                        print('Sensor Not found')
