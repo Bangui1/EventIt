49e05ad47390e58d06066ded1ca063e7df0f0c5c
AlanFierr0 <85326611+AlanFierr0@users.noreply.github.com>
2021-11-05 00:31:56 -0300

    login sin repeticiones cuil, phone y user

--- login.py
+++ login.py
@@ -7,7 +7,7 @@ class Start:
         while not log:
             username = input('Enter Username: ').rstrip()
             password = input('Enter Password: ')
-            with open('Datasets\\User_database.csv', 'r') as database:
+            with open('EventIt\\Datasets\\User_database.csv', 'r') as database:
                 for line in database:
                     row = line.strip().split(',')
                     if username == row[2].strip():
@@ -19,49 +19,96 @@ class Start:
                         print ('Username Not found')
 
 
+   
+        
+    def checkCuil(self):
+        with open('EventIt\\Datasets\\User_database.csv', 'r') as user_database:
+            check_1 = False
+            while check_1 == False:
+                cuil = input('Please enter your CUIL: ')
+                for i in user_database:
+                    row = i.strip().split(',')
+                    if cuil == row[0].strip():
+                        print ("Cuil already exist, try another one")
+                        Start.checkCuil(self)
+                    else: 
+                        check_1 = True
+                        return cuil
+
+                       
+
+        
+    def checkPhoneNumber(self):                                
+        with open('EventIt\\Datasets\\User_database.csv', 'r') as user_database:
+            check_2 = False
+            while check_2 == False:
+                phone_number = input('Please enter your phone number: ')    
+                for x in user_database:
+                    row = x.strip().split(',')
+                    if phone_number == row[1].strip():
+                        print ("phone number already exist, try another one")
+                        Start.checkPhoneNumber(self)
+                    else: 
+                        check_2 = True
+                        return phone_number
+                    
+
+    def checkUsername(self):     
+        with open('EventIt\\Datasets\\User_database.csv', 'r') as user_database:
+            check_3 = False
+            while check_3 == False:
+                username = input('Enter your username: ')
+                for y in user_database:
+                    row = y.strip().split(',')
+                    if username == row[2].strip():
+                        print ("Username already exist, try another one")
+                        Start.checkUsername(self)
+                    else: 
+                        check_3 = True
+                        return username
+                    
+
+    def checkPassword(self):      
+        check_4 = False
+        while check_4 == False:
+            password = input('Enter your password: ')
+            confirm_password = input('Please re-enter your password:')
+            if password != confirm_password:
+                print('Passwords do not match, please try again.')
+                Start.checkPassword(self)
+            else: 
+                check_4 = True
+                return password
+                
+
     def Register(self):
-        register = False
-        while not register:
-            cuil = input('Please enter your CUIL: ')
-            phone_number = input('Please enter your phone number: ')
-            username = input('Enter your username: ')
-            
-            check_password = False
-            while not check_password:
-                password = input('Enter your password: ')
-                confirm_password = input('Please re-enter your password:')
-                if password == confirm_password:
-                    check_password = True
-                else:
-                    print('Passwords do not match, please try again.')
-
-
-            with open('Datasets\\dataset_Anses.csv', 'r', newline = '') as database:
-                for line in database:
-                    row = line.strip().split(',')
-                    if cuil == row[0]:
-                        with open('Datasets\\User_database.csv', 'a') as user_database:
-                            user_data = [cuil, phone_number, username, password]
-                            data_writer = writer(user_database, lineterminator='\r')
-                            data_writer.writerow(user_data)
-                            register = True
-                            break
-            if register:
-                print('successful registration')
-            else:
-                print('CUIL not found in dataset, please try again')
+        cuil = Start.checkCuil(self)
+        phone_number = Start.checkPhoneNumber(self)
+        username = Start.checkUsername(self)
+        password = Start.checkPassword(self) 
+        cuil
+        phone_number
+        username
+        password
+        with open('EventIt\\Datasets\\User_database.csv', 'r', newline = '') as database:
+            for line in database:
+                with open('EventIt\\Datasets\\User_database.csv', 'a') as user_database:
+                    user_data = [cuil, phone_number, username, password]
+                    data_writer = writer(user_database, lineterminator='\r')
+                    data_writer.writerow(user_data)
+                    break
 
 
     def LoginAdmin(self):
         log = False
         while not log:
-            username = input('Enter Username: ').rstrip()
-            password = input('Enter Password: ')
-            with open('Datasets\\Admin_dataset.csv', 'r') as database:
+            username = input('Enter username: ').rstrip()
+            password = input('Enter password: ')
+            with open('EventIt\\Datasets\\User_database.csv', 'r') as database:
                 for line in database:
                     row = line.strip().split(',')
-                    if username == row[0].strip():
-                        if password == row[1].strip():
+                    if username == row[2].strip():
+                        if password == row[3].strip():
                             log = True
                         else:
                             print('wrong password, please try again.')
@@ -73,7 +120,7 @@ class Start:
         log = False
         while not log:
             username = input('Enter Area Code: ').rstrip()
-            with open('Datasets\\Sensor_dataset.csv', 'r') as database:
+            with open('EventIt\\Datasets\\User_database.csv', 'r') as database:
                 for line in database:
                     row = line.strip().split(',')
                     if username == row[0].strip():
