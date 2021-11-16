9fe9c2b119a40296f1e3d702f64e6d695c42c061
ChinoCoder <91900687+ChinoCoder@users.noreply.github.com>
2021-11-16 15:31:18 -0300

    mapa y eventos

--- Classes/event.py
+++ Classes/event.py
@@ -1,11 +1,30 @@
 class event:
-    def __init__(self, tipo, coords):
+    def __init__(self, tipo, zone, interesados):
         self.tipo = tipo
-        self.coords = coords #estilo (a,b) o como se maneje el mapa
+        self.zone = zone #estilo (a,b) o como se maneje el mapa
+        self.interesados = []
+    
     
     def getType(self):
         return self.type
 
+
+
+
+
+    @classmethod
+    def create_event(cls, tipo, zone):
+        return cls(tipo, zone)
+        
+#idea de solicitud de event (usuario)
+# def solicitar_evento(self):
+#     with open('event_types.csv')
+#     seleccionar_tipo = input('ingrese el numero')
+#     return event.create_event(seleccionar_tipo, self.zone)
+
+
+
+        
 class sensor:
     # def __init__(self):
     #     self.listaSensores = [] #esto podria ir en el login 
--- Classes/map.py
+++ Classes/map.py
@@ -1,6 +1,14 @@
-import folium, 
+import folium, pandas, webbrowser
 
-mapa = folium.Map()
+mapa = folium.Map(location=[-36.233913, -60.645759], zoom_start=7)
+    
+    
+marker_csv = pandas.read_csv('Datasets\Zone_markers.csv')
+for i in range(0, len(marker_csv)):
+    folium.Marker(location= [marker_csv.iloc[i]['lat'], marker_csv.iloc[i]['lng']], popup= marker_csv.iloc[i]['Nombre']).add_to(mapa)
+
+mapa.save('mapa.html')
+webbrowser.open('mapa.html')
 
 
 
