0238206302084605947196f7700cc52ce5f001fd
ChinoCoder <91900687+ChinoCoder@users.noreply.github.com>
2021-11-16 14:00:19 -0300

    datos mapa

--- Classes/map.py
+++ Classes/map.py
@@ -1,8 +1,10 @@
 
-class mapa:
-    #algun tipo de grid o matriz con coordenadas
-    pass
 
-class zona:
-    #cada zona es tiene sus propias coordenadas en el mapa
-    pass
+
+# class mapa:
+#     #algun tipo de grid o matriz con coordenadas
+#     pass
+
+# class zona:
+#     #cada zona es tiene sus propias coordenadas en el mapa
+#     pass
