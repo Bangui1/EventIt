a1d96ee8eeb9068a3d112cc315687acb4219e5cc
ChinoCoder <91900687+ChinoCoder@users.noreply.github.com>
2021-11-18 18:31:03 -0300

    mapa

--- Classes/map.py
+++ Classes/map.py
@@ -1,11 +1,25 @@
-import folium, pandas, webbrowser
+import folium 
+import pandas 
+import webbrowser
 
 mapa = folium.Map(location=[-36.233913, -60.645759], zoom_start=7)
     
-    
 marker_csv = pandas.read_csv('Datasets\Zone_markers.csv')
+
+
+
 for i in range(0, len(marker_csv)):
-    folium.Marker(location= [marker_csv.iloc[i]['lat'], marker_csv.iloc[i]['lng']], popup= marker_csv.iloc[i]['Nombre']).add_to(mapa)
+    def info():
+        with open('Datasets\\Events_database.csv', 'r') as events:
+            info = ""
+            for line in events:
+                row = line.strip().split(",")
+                if marker_csv.iloc[i]['$$_Nombre_$$'] == row[1]:
+                    info = info + f"{row[2]}\n"
+            return info
+    
+    
+    folium.Marker(location= [marker_csv.iloc[i]['$$_lat_$$'], marker_csv.iloc[i]['$$_lng_$$']], popup= info()).add_to(mapa)
 
 mapa.save('mapa.html')
 webbrowser.open('mapa.html')
